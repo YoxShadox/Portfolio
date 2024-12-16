fbca20620ea31494ee637ef3b77cef0acd101085
YoxShadox <yoxshadox@gmail.com>
2024-12-17 00:15:07 +0530

    version: 0.1

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".text",
+    {
+    strings:["Frontend Devloper","UI/UX Designer","Web Devloper"],
+    typeSpeed: 80,
+    backSpeed: 100,
+    backDelay: 800,
+    loop: true
+});
+
